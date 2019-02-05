58f027b5c1112f60c1b3a330958b7dea6ad7e2b5
jelmer04 <jon.s.elmer@googlemail.com>
2019-02-05 20:39:30 +0000

    Update and rename colab.py to mathjax.py

---
+++ mathjax.py
@@ -0,0 +1,6 @@
+from IPython.display import display, Math, HTML
+
+def load_mathjax_in_cell_output():
+  display(HTML("<script src='https://www.gstatic.com/external_hosted/"
+               "mathjax/latest/MathJax.js?config=default'></script>"))
+get_ipython().events.register('pre_run_cell', load_mathjax_in_cell_output)
