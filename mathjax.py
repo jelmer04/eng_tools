a755dc9c720725c0a93d74c09476e993102f20af
jelmer04 <jon.s.elmer@googlemail.com>
2019-06-07 15:33:11 +0100

    Update mathjax.py

--- mathjax.py
+++ mathjax.py
@@ -2,5 +2,5 @@ from IPython.display import display, Math, HTML
 
 def load_mathjax_in_cell_output():
   display(HTML("<script src='https://www.gstatic.com/external_hosted/"
-               "mathjax/latest/MathJax.js?config=default'></script>"))
+               "mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML'></script>"))
 get_ipython().events.register('post_run_cell', load_mathjax_in_cell_output)
